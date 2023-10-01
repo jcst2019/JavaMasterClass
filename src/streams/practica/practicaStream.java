944535582db3e8aeda259e4cadf8cefe28548085
jcst2019 <juansolar_4@hotmail.com>
2023-10-01 06:00:33 -0500

    Practica Stream V2

--- src/streams/practica/practicaStream.java
+++ src/streams/practica/practicaStream.java
@@ -10,14 +10,20 @@ public class practicaStream {
         Stream<Videogame> myStram = Database.videogames.stream();
         System.out.println(myStram); //Me imprime el objeto
         myStram.forEach(System.out::println);
-        System.out.println("Ordenar el Stream");
+        System.out.println("Ordenar el Stream por Nombre del Videojuego");
         Stream<Videogame> myStream2 = Database.videogames.stream();
-        //Muesyra este error stream has already been operated upon or closed
+        //Muestra este error stream has already been operated upon or closed
         //myStram2.filter(v -> v.getConsole().equals(Console.XBOX));
         //myStram2.forEach(s-> System.out.println(s));
         myStream2.filter(v -> v.getConsole().equals(Console.XBOX))
                  .sorted(Comparator.comparing(Videogame::getName))
                  .forEach(System.out::println);
+
+        System.out.println("Ordenar el Stream por Precio del Videojuego");
+        Database.videogames.stream()
+                .filter(v -> v.getConsole().equals(Console.NINTENDO))
+                .sorted(Comparator.comparing(Videogame::getPrice))
+                .forEach(System.out::println);
 
     }
 }
